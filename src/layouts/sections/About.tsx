61771dad5fdc94b4391fc0123ca4140e00fed3e2
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-06-26 15:57:45 -0300

    Update About.tsx

--- src/layouts/sections/About.tsx
+++ src/layouts/sections/About.tsx
@@ -3,6 +3,11 @@ export default function About() {
   return (
     <section id='about' className="pt-52">About
       <Button variant="outline">Button</Button>
+      <p>Tras casi 10 años trabajando en empleos donde desarrollé fuertes sentidos de responsabilidad, trbajo en equipo y resolución de problemas, en 2020 encontré el
+      campo donde mezclar estas habilidades con mi pasión por aprendizaje continuo, idiomas y desarrollo de ideas creativas</p>
+      <p>Al día de hoy llevo más de 2 años trabajando profesionalmente como desarrollador frontend y creando paralelamente proyectos personales para aprender
+      nuevas tecnologías y tendencias, especilalizandome actualmente en el mundo de React.</p>
+   
     </section>
   )
 }
