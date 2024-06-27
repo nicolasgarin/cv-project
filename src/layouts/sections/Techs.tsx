6ceba020330110aaae910c3d8c09244ed8de743a
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-06-27 12:02:03 -0300

    Update Techs.tsx

--- src/layouts/sections/Techs.tsx
+++ src/layouts/sections/Techs.tsx
@@ -1,3 +1,9 @@
+import git from "../../assets/logos/git.svg";
+import html from "../../assets/logos/html-5.svg";
+import css from "../../assets/logos/css-3.svg";
+import js from "../../assets/logos/javascript.svg";
+import python from "../../assets/logos/python.svg";
+import java from "../../assets/logos/java.svg";
 import bootstrap from "../../assets/logos/bootstrap.svg";
 
 export default function Techs() {
@@ -6,10 +12,35 @@ export default function Techs() {
         <h3>Tecnologías y habilidades</h3>
         <h4>Tecnologías</h4>
         <div className="flex">
-            <div className="flex flex-column justify-center">
-                <img className="h-12" src={bootstrap} />
-                <div>Bootstrap</div>
+          <div className="nivel-1 flex justify-around">
+            <div className="flex flex-column">
+                <img className="h-12" src={git} />
+                <div>Git</div>
             </div>
+            <div className="flex flex-column">
+                <img className="h-12" src={html} />
+                <div>HTML 5</div>
+            </div>
+            <div className="flex flex-column">
+                <img className="h-12" src={css} />
+                <div>Css 3</div>
+            </div>
+            <div className="flex flex-column">
+                <img className="h-12" src={js} />
+                <div>Javascript</div>
+            </div>
+            <div className="flex flex-column">
+                <img className="h-12" src={python} />
+                <div>Python</div>
+            </div>
+            <div className="flex flex-column">
+                <img className="h-12" src={java} />
+                <div>Java</div>
+            </div>
+          </div>
+          <div className="nivel-2"></div>
+          <div className="nivel-3"></div>
+          <div className="nivel-4"></div>
         </div>
         </section>
     )
