5ca6a3c7a7b9a8a60b53961aaa7c30e59d8d37fb
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-06-27 09:10:01 -0300

    Update Techs.tsx

--- src/layouts/sections/Techs.tsx
+++ src/layouts/sections/Techs.tsx
@@ -1,6 +1,17 @@
+import bootstrap from "../../assets/logos/bootstrap.svg";
 
 export default function Techs() {
-  return (
-    <section id='techs'>Techs</section>
-  )
-}
+    return (
+      <section id='techs'>
+        <h3>Tecnologías y habilidades</h3>
+        <h4>Tecnologías</h4>
+        <div className="flex">
+            <div>
+                <img src={bootstrap} />
+                <div>Bootstrap</div>
+            </div>
+        </div>
+        </section>
+    )
+  }
+  
