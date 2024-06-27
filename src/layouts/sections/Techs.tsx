6b3797f499ca8c126b7852efdaaea1de473ad742
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-06-27 13:59:50 -0300

    Update Techs.tsx

--- src/layouts/sections/Techs.tsx
+++ src/layouts/sections/Techs.tsx
@@ -29,101 +29,101 @@ export default function Techs() {
       <h4>Tecnologías</h4>
       <div className="flex flex-col">
         <div className="nivel-1 flex justify-around">
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={git} />
             <div>Git</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={html} />
             <div>HTML 5</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={css} />
             <div>Css 3</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={js} />
             <div>Javascript</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={python} />
             <div>Python</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={java} />
             <div>Java</div>
           </div>
         </div>
         <div className="nivel-2 flex justify-around">
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={github} />
             <div>Github</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={figma} />
             <div>Figma</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={sass} />
             <div>Sass</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={react} />
             <div>React</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={ts} />
             <div>Typescript</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={jq} />
             <div>Jquery</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={freemarker} />
             <div>Freemarker</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={clay} />
             <div>Clay UI</div>
           </div>
         </div>
         <div className="nivel-3 flex justify-around">
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={mysql} />
             <div>Mysql</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={vite} />
             <div>Vite</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={bootstrap} />
             <div>Bootstrap</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={tailwind} />
-            <div>Tailwind</div>
+            <div>Tailwind CSS</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={liferay} />
             <div>Liferay</div>
           </div>
         </div>
         <div className="nivel-4 flex justify-around">
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={vitest} />
             <div>Vitest</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={bootswatch} />
             <div>Bootswatch</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={daisy} />
             <div>DaisyUI</div>
           </div>
-          <div className="flex flex-col">
+          <div className="ic flex flex-col">
             <img className="h-12" src={shadcn} />
             <div>Shadcn/ui</div>
           </div>
