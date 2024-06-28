02c48378b93456c7ebf0801da27b2e13245d90d6
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-06-27 23:27:39 -0300

    .

--- src/layouts/sections/Techs.tsx
+++ src/layouts/sections/Techs.tsx
@@ -1,5 +1,5 @@
-import git from "/git.svg";
-import html from "/html-5.svg";
+import git from "../../assets/logos/git.svg";
+import html from "../../assets/logos/html-5.svg";
 import css from "../../assets/logos/css-3.svg";
 import js from "../../assets/logos/javascript.svg";
 import python from "../../assets/logos/python.svg";
@@ -23,124 +23,150 @@ import daisy from "../../assets/logos/daisyui.svg";
 import shadcn from "../../assets/logos/shadcn-ui.svg";
 import radix from "../../assets/logos/radix.svg";
 import nextjs from "../../assets/logos/next-js.svg";
-import { ReactSVG } from 'react-svg';
+import { ReactSVG } from "react-svg";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../../components/ui/tooltip";
 
 export default function Techs() {
   return (
     <section id="techs">
       <div className="flex flex-col justify-center h-full">
-      <h3>Tecnologías</h3>
-      <div className="flex flex-col gap-6">
-        <div className="nivel-1 flex justify-around">
-		<div className="ic flex flex-col">
-            <ReactSVG src={git} />
-            <div>Git</div>
-          </div>
-          <div className="ic flex flex-col">
-            <ReactSVG src={html} />
-            <div>HTML 5</div>
+        <h3>Tecnologías</h3>
+        <div className="flex flex-col gap-6">
+          <div className="nivel-1 flex justify-around">
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <div className="ic flex flex-col">
+                    <ReactSVG src={git} />
+                    <div className="text-center">Git</div>
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p className="text-cyan-400">Esto es git</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider>
+  <Tooltip>
+    <TooltipTrigger>            <div className="ic flex flex-col">
+              <ReactSVG src={html} />
+              <div className="text-center">HTML 5</div>
+            </div></TooltipTrigger>
+    <TooltipContent className="bg-slate-500">
+      <p className="text-black">eto es html</p>
+      <p className="text-black">eto es html</p>
+      <p className="text-black">eto es html</p>
+      <p className="text-black">eto es html</p>
+    </TooltipContent>
+  </Tooltip>
+</TooltipProvider>
 
+
+            <div className="ic flex flex-col">
+              <ReactSVG src={css} />
+              <div className="text-center">Css 3</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={js} />
+              <div className="text-center">Javascript</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={python} />
+              <div className="text-center">Python</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={java} />
+              <div className="text-center">Java</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={mysql} />
+              <div className="text-center">Mysql</div>
+            </div>
           </div>
-          <div className="ic flex flex-col">
-          <ReactSVG src={css} />
-            <div>Css 3</div>
+          <div className="nivel-2 flex justify-around">
+            <div className="ic flex flex-col">
+              <ReactSVG src={github} />
+              <div className="text-center">Github</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={figma} />
+              <div className="text-center">Figma</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={sass} />
+              <div className="text-center">Sass</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={react} />
+              <div className="text-center">React</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={ts} />
+              <div className="text-center">Typescript</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={jq} />
+              <div className="text-center">Jquery</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={freemarker} />
+              <div className="text-center">Freemarker</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={clay} />
+              <div className="text-center">Clay UI</div>
+            </div>
           </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={js} />
-            <div>Javascript</div>
+          <div className="nivel-3 flex justify-around">
+            <div className="ic flex flex-col">
+              <ReactSVG src={vite} />
+              <div className="text-center">Vite</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={bootstrap} />
+              <div className="text-center">Bootstrap</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={tailwind} />
+              <div className="text-center">Tailwind CSS</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={radix} />
+              <div className="text-center">Radix</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={nextjs} />
+              <div className="text-center">Next.js</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={liferay} />
+              <div className="text-center">Liferay</div>
+            </div>
           </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={python} />
-            <div>Python</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={java} />
-            <div>Java</div>
-          </div>
-		  <div className="ic flex flex-col">
-            <img className="h-12" src={mysql} />
-            <div>Mysql</div>
+          <div className="nivel-4 flex justify-around">
+            <div className="ic flex flex-col">
+              <ReactSVG src={vitest} />
+              <div className="text-center">Vitest</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={bootswatch} />
+              <div className="text-center">Bootswatch</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={daisy} />
+              <div className="text-center">DaisyUI</div>
+            </div>
+            <div className="ic flex flex-col">
+              <ReactSVG src={shadcn} />
+              <div className="text-center">Shadcn/ui</div>
+            </div>
           </div>
         </div>
-        <div className="nivel-2 flex justify-around">
-		<div className="ic flex flex-col">
-            <img className="h-12" src={github} />
-            <div>Github</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={figma} />
-            <div>Figma</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={sass} />
-            <div>Sass</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={react} />
-            <div>React</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={ts} />
-            <div>Typescript</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={jq} />
-            <div>Jquery</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={freemarker} />
-            <div>Freemarker</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={clay} />
-            <div>Clay UI</div>
-          </div>
-        </div>
-        <div className="nivel-3 flex justify-around">
-          <div className="ic flex flex-col">
-            <img className="h-12" src={vite} />
-            <div>Vite</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={bootstrap} />
-            <div>Bootstrap</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={tailwind} />
-            <div>Tailwind CSS</div>
-          </div>
-		  <div className="ic flex flex-col">
-            <img className="h-12" src={radix} />
-            <div>Radix</div>
-          </div>
-		  <div className="ic flex flex-col">
-            <img className="h-12" src={nextjs} />
-            <div>Next.js</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={liferay} />
-            <div>Liferay</div>
-          </div>
-        </div>
-        <div className="nivel-4 flex justify-around">
-          <div className="ic flex flex-col">
-            <img className="h-12" src={vitest} />
-            <div>Vitest</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={bootswatch} />
-            <div>Bootswatch</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={daisy} />
-            <div>DaisyUI</div>
-          </div>
-          <div className="ic flex flex-col">
-            <img className="h-12" src={shadcn} />
-            <div>Shadcn/ui</div>
-          </div>
-        </div>
-      </div>
       </div>
     </section>
   );
