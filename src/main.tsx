87834bf751ebc90498526f37e35ab77ac4753b66
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-07-17 11:02:49 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 import "./main.css";
 import "./styles/techsTree.scss"
 import { UserOptionsProvider } from "./context/UserOptionsContext.tsx";
+import { DataProvider } from "./context/DataContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <UserOptionsProvider>
-      <App />
+      <DataProvider>
+        <App />
+      </DataProvider>
     </UserOptionsProvider>
   </React.StrictMode>
 );
