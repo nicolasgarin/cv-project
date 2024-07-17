52dfbe901d2c58dd87ee31a7f872b21e29dbf64c
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-07-17 16:02:40 -0300

    Update About.tsx

--- src/layouts/sections/About.tsx
+++ src/layouts/sections/About.tsx
@@ -13,8 +13,8 @@ export default function About() {
               Tras 10 años trabajando en empleos donde desarrollé fuertes
               sentidos de responsabilidad, trabajo en equipo y adaptación
               constante, en 2020 encontré el campo donde mezclar estas
-              habilidades con mi pasión por el aprendizaje, la resolución
-              creativa de problemas y los idiomas.
+              habilidades con mi pasión por el aprendizaje y la resolución
+              creativa de problemas.
             </p>
           ) : (
             <p className="mb-4">
@@ -28,7 +28,7 @@ export default function About() {
           {lang == "es" ? (
             <p className="mb-4">
               Al día de hoy llevo más de 2 años trabajando profesionalmente como
-              desarrollador frontend y creando paralelamente proyectos personale
+              desarrollador frontend y creando paralelamente proyectos personales
               para aprender nuevas tecnologías y tendencias, especilalizandome
               actualmente en el ecosistema de React.
             </p>
@@ -42,10 +42,9 @@ export default function About() {
           )}
           {lang == "es" ? (
             <p>
-              {" "}
               Tengo experiencia trabajando tanto en frontend como en backend, y
               amplio conocimiento en los conceptos y aplicación de metodologías
-              ágiles, accesibilidad y diseño responsive.
+              ágiles, accesibilidad y diseño responsive que las web modernas necesitan.
             </p>
           ) : (
             <p>
